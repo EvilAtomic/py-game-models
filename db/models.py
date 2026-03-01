404c132772e7995a825546c792e23a528ba4d75b
artem <artemasarkisan598@gmail.com>
2026-03-01 16:15:21 +0200

    py-Game Models

--- db/models.py
+++ db/models.py
@@ -9,7 +9,8 @@ class Race(models.Model):
 class Skill(models.Model):
     name = models.CharField(max_length=255, unique=True)
     bonus = models.CharField(max_length=255)
-    race = models.ForeignKey(Race, on_delete=models.CASCADE)
+    race = models.ForeignKey(Race, on_delete=models.CASCADE,
+                             related_name="skills")
 
 
 class Guild(models.Model):
