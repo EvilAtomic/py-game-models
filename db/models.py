fe11194322f1b69374dd52d479ba49750bd1419e
artem <artemasarkisan598@gmail.com>
2026-03-01 16:09:48 +0200

    py-Game Models

--- db/models.py
+++ db/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 class Race(models.Model):
     name = models.CharField(max_length=255, unique=True)
     description = models.TextField(blank=True)
@@ -21,5 +22,6 @@ class Player(models.Model):
     email = models.EmailField(max_length=255)
     bio = models.CharField(max_length=255)
     race = models.ForeignKey(Race, on_delete=models.CASCADE)
-    guild = models.ForeignKey(Guild, on_delete=models.SET_NULL, null=True, blank=True)
+    guild = models.ForeignKey(Guild, on_delete=models.SET_NULL,
+                              null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
